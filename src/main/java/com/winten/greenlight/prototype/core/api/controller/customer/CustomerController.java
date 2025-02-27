4825b7c769e30901b84c5c2d8d2803243bfc5c76
LSMJJAng <127660101+LSMJJAng@users.noreply.github.com>
2025-02-27 21:07:07 +0900

    create Customer 2차

--- src/main/java/com/winten/greenlight/prototype/core/api/controller/customer/CustomerController.java
+++ src/main/java/com/winten/greenlight/prototype/core/api/controller/customer/CustomerController.java
@@ -22,37 +22,38 @@ public class CustomerController {
     // TODO [사용자] 이벤트 대기열 참가신청 API https://github.com/B2-2-BW/greenlight-prototype-core-api/issues/2
     @PostMapping("")
     public Mono<ResponseEntity<CustomerRegistrationResponseDto>> createCustomer(@RequestBody final CustomerRequestDto requestDto) {
+        long score = System.currentTimeMillis(); // 선착순 보장을 위해 최상단에서 score 채번
+
         return Mono.defer(() -> {
-                    long score = System.currentTimeMillis(); // 선착순 보장을 위해 최상단에서 score 채번
-                    Customer customer = new Customer();
-                    customer.setScore(score);
+                Customer customer = new Customer();
+                customer.setScore(score);
 
-                    Event event = new Event();
-                    event.setEventName(requestDto.getEventName()); // 이벤트 객체 생성
+                Event event = new Event();
+                event.setEventName(requestDto.getEventName()); // 이벤트 객체 생성
 
-                    return customerService.createCustomer(customer, event)
-                            .map(responseCustomer -> ResponseEntity
-                                    .status(201) // 201 CREATED 반환
-                                    .body(new CustomerRegistrationResponseDto(
-                                            responseCustomer.getCustomerId(),
-                                            responseCustomer.getScore(),
-                                            new CustomerQueueInfo()
-                                    )));
-                })
-                .doOnSuccess(response -> log.info("Customer created successfully: {}", response))
-                .doOnError(error -> log.error("Error while creating customer", error))
-                .onErrorResume(error -> {
-                    if (error instanceof IllegalArgumentException) {
-                        return Mono.just(ResponseEntity
-                                .badRequest()
-                                .body(new CustomerRegistrationResponseDto(null, 0, null))
-                        );
-                    }
+                return customerService.createCustomer(customer, event)
+                        .map(responseCustomer -> ResponseEntity
+                                .status(201) // 201 CREATED 반환
+                                .body(new CustomerRegistrationResponseDto(
+                                        responseCustomer.getCustomerId(),
+                                        responseCustomer.getScore(),
+                                        new CustomerQueueInfo()
+                                )));
+            })
+            .doOnSuccess(response -> log.info("Customer created successfully: {}", response))
+            .doOnError(error -> log.error("Error while creating customer", error))
+            .onErrorResume(error -> {
+                if (error instanceof IllegalArgumentException) {
                     return Mono.just(ResponseEntity
-                            .status(500)
+                            .badRequest()
                             .body(new CustomerRegistrationResponseDto(null, 0, null))
                     );
-                });
+                }
+                return Mono.just(ResponseEntity
+                        .status(500)
+                        .body(new CustomerRegistrationResponseDto(null, 0, null))
+                );
+            });
     }
 
     // TODO [사용자] 이벤트 대기상태 조회 API https://github.com/B2-2-BW/greenlight-prototype-core-api/issues/3
--- src/main/java/com/winten/greenlight/prototype/core/db/repository/redis/customer/CustomerRepository.java
+++ src/main/java/com/winten/greenlight/prototype/core/db/repository/redis/customer/CustomerRepository.java
@@ -4,6 +4,8 @@ import com.github.f4b6a3.tsid.TsidCreator;
 import com.winten.greenlight.prototype.core.domain.customer.Customer;
 import com.winten.greenlight.prototype.core.domain.customer.CustomerQueueInfo;
 import com.winten.greenlight.prototype.core.domain.customer.WaitingPhase;
+import com.winten.greenlight.prototype.core.support.error.CoreException;
+import com.winten.greenlight.prototype.core.support.error.ErrorType;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Mono;
@@ -28,13 +30,10 @@ public class CustomerRepository {
 //                            log.info("Successfully saved ticket: {}", newCustomer);
                             sink.next(newCustomer);
                         } else {
-                            sink.error(new RuntimeException("Failed to save customer in Redis."));
+                            sink.error(CoreException.of(ErrorType.REDIS_ERROR, "Customer Not Found"));
                         }
                     })
-                    .onErrorResume(e -> {
-//                        log.error("Failed to save ticket", e);
-                        return Mono.error(e);
-                    });
+                    .onErrorResume(e -> Mono.error(CoreException.of(ErrorType.REDIS_ERROR, e.getMessage())));
         });
     }
 
