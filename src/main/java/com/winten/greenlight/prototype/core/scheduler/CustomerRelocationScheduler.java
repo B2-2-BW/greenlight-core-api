6592cbf42659707cff33e23c2d8cdbd3c95da0c0
ball4716 <ball4716@naver.com>
2025-03-11 11:25:14 +0900

    [FO 코어 API] [공통] 스케줄러 기능 개선
    CustomerRelocationScheduler.java: scheduleCustomerRelocation() 추가
    CustomerRepository.java: getTopNCustomers() 추가
    CustomerService.java: relocateCustomer() 추가
    createCustomer() 메소드, Repository에는 DB호출 관련 로직만 있도록, 비즈니스로직 Service로 이관
    
    기능 분할 및 nonBlocking 하도록 구조 개선

--- src/main/java/com/winten/greenlight/prototype/core/scheduler/CustomerRelocationScheduler.java
+++ src/main/java/com/winten/greenlight/prototype/core/scheduler/CustomerRelocationScheduler.java
@@ -1,5 +1,6 @@
 package com.winten.greenlight.prototype.core.scheduler;
 
+import com.winten.greenlight.prototype.core.domain.customer.CustomerService;
 import com.winten.greenlight.prototype.core.domain.customer.WaitingPhase;
 import com.winten.greenlight.prototype.core.domain.event.CachedEventService;
 import com.winten.greenlight.prototype.core.domain.event.Event;
@@ -18,13 +19,15 @@ import java.time.Duration;
 @Component
 public class CustomerRelocationScheduler {
     private final CachedEventService cachedEventService;
+    private final CustomerService customerService;
     private final ReactiveRedisTemplate<String, String> reactiveRedisTemplate;
     private Event event;
     private String eventName = "test";
 
 
-    public CustomerRelocationScheduler(CachedEventService cachedEventService, ReactiveRedisTemplate<String, String> reactiveRedisTemplate) {
+    public CustomerRelocationScheduler(CachedEventService cachedEventService, CustomerService customerService, ReactiveRedisTemplate<String, String> reactiveRedisTemplate) {
         this.cachedEventService = cachedEventService;
+        this.customerService = customerService;
         this.reactiveRedisTemplate = reactiveRedisTemplate;
         this.event = new Event();
         event.setEventName(eventName);
@@ -39,39 +42,7 @@ public class CustomerRelocationScheduler {
                     //0) N을 가져온다 : CachedEventService를 이용
                     return cachedEventService.getEventByName(event)
                                     .map(Event::getQueueBackpressure)
-                                    .flatMapMany(n -> {
-                                        if(n==0){ // 넘기는 고객의 수가 0일 경우, Flux.empty를 반환하고 종료한다.
-                                            log.info("No customers to relocated. Skipping transaction");
-                                            return Flux.empty();
-                                        }
-                                        //1) watingQueue에서 상위 N명의 고객을 가져온다
-                                        return reactiveRedisTemplate.opsForZSet().rangeWithScores(WaitingPhase.WAITING.queueName(), Range.closed(0L, ((long)n-1L)))
-                                                .collectList()
-                                                .flatMapMany(customers -> {
-                                                    //전체 customers의 List를 기준으로 반복문을 실행 한다.
-                                                    return Flux.fromIterable(customers)
-                                                            .flatMap(customer -> Mono.justOrEmpty(customer.getValue())
-                                                                    .filter(value -> !value.isEmpty())
-                                                                    .flatMap(value -> {
-                                                                        double score = customer.getScore();
-                                                                        return reactiveRedisTemplate.opsForZSet().add(WaitingPhase.READY.queueName(), value, score)//ready Queue insert
-                                                                                .flatMap(success -> {
-                                                                                    if(Boolean.TRUE.equals(success)) {
-                                                                                        return reactiveRedisTemplate.opsForZSet().remove(WaitingPhase.WAITING.queueName(), value)
-                                                                                                .doOnSuccess(result -> log.info("Successfully relocated customer {}: {}", value, result));
-                                                                                    } else {
-                                                                                        log.warn("Failed to add customer to READY queue: {}. Skipping transaction", value);
-                                                                                        return Mono.empty();
-                                                                                    }
-                                                                                })
-                                                                                .onErrorResume(error -> {
-                                                                                    log.error("Error relocating customer {}: {}", value, error.getMessage());
-                                                                                    return Mono.empty();
-                                                                                });
-                                                                    })
-                                                            );
-                                                });
-                                    });
+                                    .flatMapMany(customerService::relocateCustomer);
                 })
                 .doOnError(error -> log.error("Scheduler encountered an error: {}", error.getMessage(), error))
                 .onErrorContinue((error, obj) -> log.warn("Ignoring error and continuing: {}", error.getMessage()))
