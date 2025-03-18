14d114bbab4a6b18e6aaed4b6046cdcb30e52619
Daniel Choi <danielchoi1115@gmail.com>
2025-03-18 16:44:23 +0900

    Cache 초기화 시 notfound 발생하는 문제 수정

--- src/main/java/com/winten/greenlight/prototype/core/api/controller/event/EventController.java
+++ src/main/java/com/winten/greenlight/prototype/core/api/controller/event/EventController.java
@@ -28,6 +28,7 @@ public class EventController {
     @DeleteMapping("/{eventName}/cache")
     public Mono<ResponseEntity<EventResponseDto>> invalidateEventCache(@BindParam final EventRequestDto requestDto) {
         return cachedEventService.invalidateEventCache(requestDto.toEvent())
-                .flatMap(event -> Mono.just(ResponseEntity.ok(new EventResponseDto(event))));
+                .flatMap(event -> Mono.just(ResponseEntity.ok(new EventResponseDto(event))))
+                .switchIfEmpty(Mono.just(ResponseEntity.ok(new EventResponseDto())));
     }
 }
