7ea91d3650bd5dd54678e7e64780c57a2da258a9
Choi Seung Ryeol <danielchoi1115@gmail.com>
2025-05-20 14:26:25 +0900

    Feat: redis cluster 설정 변경

--- src/main/java/com/winten/greenlight/prototype/core/db/config/CoreRedisConfig.java
+++ src/main/java/com/winten/greenlight/prototype/core/db/config/CoreRedisConfig.java
@@ -2,18 +2,25 @@ package com.winten.greenlight.prototype.core.db.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.winten.greenlight.prototype.core.db.repository.redis.event.EventEntity;
+import io.lettuce.core.ReadFrom;
+import io.lettuce.core.cluster.ClusterClientOptions;
+import io.lettuce.core.cluster.ClusterTopologyRefreshOptions;
 import io.lettuce.core.resource.ClientResources;
 import io.lettuce.core.tracing.MicrometerTracing;
 import io.micrometer.observation.ObservationRegistry;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.ReactiveRedisOperations;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.data.redis.serializer.*;
+
+import java.time.Duration;
+import java.util.List;
 
 @Configuration
 public class CoreRedisConfig {
@@ -26,18 +33,34 @@ public class CoreRedisConfig {
     }
     @Bean
     public LettuceConnectionFactory lettuceConnectionFactory(RedisProperties properties, ClientResources clientResources) {
-        var config = new RedisStandaloneConfiguration(properties.getHost(), properties.getPort());
-        config.setPassword(properties.getPassword());
+        var clusterNodes = properties.getCluster().getNodes();
+        var clusterConfig = new RedisClusterConfiguration(clusterNodes);
+        clusterConfig.setPassword(properties.getPassword());
+
+        var topologyRefreshOptions = ClusterTopologyRefreshOptions.builder()
+                .enablePeriodicRefresh(Duration.ofSeconds(30)) // 주기적으로 토폴로지 새로고침
+                .enableAllAdaptiveRefreshTriggers() // MOVED, ASK 등 다양한 트리거에 반응하여 새로고침
+                .adaptiveRefreshTriggersTimeout(Duration.ofSeconds(25)) // 적응형 새로고침 타임아웃
+                .build();
+
+        var clusterClientOptions = ClusterClientOptions.builder()
+                .topologyRefreshOptions(topologyRefreshOptions)
+                .build();
+
         var clientConfig = LettuceClientConfiguration.builder()
                 .clientResources(clientResources)
+                .clientOptions(clusterClientOptions)
+                .readFrom(ReadFrom.REPLICA_PREFERRED) // 읽기 작업을 슬레이브 노드에서 수행하도록 설정 (선택 사항)
+                .commandTimeout(Duration.ofSeconds(10)) // 커맨드 타임아웃 설정
                 .build();
-        return new LettuceConnectionFactory(config, clientConfig);
+
+        return new LettuceConnectionFactory(clusterConfig, clientConfig);
     }
 
     @Bean
     public ReactiveRedisOperations<String, String> reactiveRedisTemplate(LettuceConnectionFactory factory) {
-        StringRedisSerializer serializer = new StringRedisSerializer();
-        RedisSerializationContext<String, String> serializationContext = RedisSerializationContext
+        var serializer = new StringRedisSerializer();
+        var serializationContext = RedisSerializationContext
                 .<String, String>newSerializationContext()
                 .key(serializer)
                 .value(serializer)
