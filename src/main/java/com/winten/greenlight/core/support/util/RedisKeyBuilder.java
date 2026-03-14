faedd7abc5ce12194e3609d8f7a08cd791ceb35c
CHOI, Seung Ryeol <schoiak@connect.ust.hk>
2026-03-14 16:00:35 +0900

    예상대기시간 기능 추가

--- src/main/java/com/winten/greenlight/core/support/util/RedisKeyBuilder.java
+++ src/main/java/com/winten/greenlight/core/support/util/RedisKeyBuilder.java
@@ -98,4 +98,8 @@ public class RedisKeyBuilder {
     public String siteInfoMeta(String siteId) {
         return prefix + ":site:" + siteId + ":meta";
     }
+
+    public String roomMetricLatest(String roomId) {
+        return prefix + ":room:{" + roomId + "}:metric:latest";
+    }
 }
